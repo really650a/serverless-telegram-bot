fd66b0cdfce6880bbf5c774e2f6a13d203512f07
really650a <99811385+really650a@users.noreply.github.com>
2023-08-06 16:46:52 +0300

    Update app.py

--- api/app.py
+++ api/app.py
@@ -4,42 +4,22 @@ import requests
 from flask import Flask
 from flask import request
 from flask import Response
+from telebot import TeleBot 
+from telebot.types import *
 
+TOKEN = "6457745689:AAGK_N4F-8KPw7zpnGf8NfFZrpTD2RhkotM"
+bot = TeleBot(TOKEN, parse_mode="html")
 app = Flask(__name__)
-TOKEN = "5769907387:AAF0tVVa2RNQjFpOeYmRAIWBhzIBa1jFp4E"
 
-def imageAsDict(imageURL, caption):
-    return {
-        "type": "photo",
-        "media": imageURL,
-        "caption": caption,
-    }
-
-
-def sendMediaGroup(chatid, allImages):
-    url = f"https://api.telegram.org/bot{TOKEN}/sendMediaGroup"
-    media = [imageAsDict(allImages[i]["src"], allImages[i]["prompt"]) for i in range(5)]
-    payload = {"chat_id": chatid, "media": media}
-    r = requests.post(url, json=payload)
-    return r
-
-def sendMessage(chat_id, text):
-    url = f"https://api.telegram.org/bot{TOKEN}/sendMessage"
-    payload = {"chat_id": chat_id, "text": text}
-    r = requests.post(url, json=payload)
-    return r
+@bot.message_handler(commands =["start"])
+def start(message):
+	bot.send_message(message.chat.id, "Hello!")
 
 @app.post("/")
 def index():
-    msg = request.get_json()
-    chat_id = msg["message"]["chat"]["id"]
-    inputText = msg["message"]["text"]
-    if inputText == "/start":
-        sendMessage(chat_id, "Ya, I am Online. Send me a Prompt")
-    else:
-        BASE_URL = "https://lexica.art/api/v1/search?q=" + str(inputText)
-        response = requests.get(BASE_URL)
-        response_text = json.loads(response.text)
-        allImages = response_text["images"]
-        sendMediaGroup(chat_id, allImages)
-    return Response("ok", status=200)
+    update = Update.de_json(request.stream.read().decode('utf-8'))
+    if update.message:
+        bot.process_new_messages([update.message])
+        
+    return "Hello World!", 200
+
