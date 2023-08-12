8a562f94e9e3f97a81e4783718400a36dc2898d4
really650a <99811385+really650a@users.noreply.github.com>
2023-08-12 19:10:08 +0300

    Update app.py

--- api/app.py
+++ api/app.py
@@ -2,17 +2,27 @@ from flask import Flask, request
 import telebot
 from telebot.types import *
 
-TOKEN = "6457745689:AAGK_N4F-8KPw7zpnGf8NfFZrpTD2RhkotM"
+TOKEN = "5769907387:AAF0tVVa2RNQjFpOeYmRAIWBhzIBa1jFp4E"
+server = Flask(__name__)
+
 bot = telebot.TeleBot(TOKEN, parse_mode="html")
-app = Flask(__name__)
 
-@bot.message_handler(commands =["start"])
+@bot.message_handler(commands=["start"])
 def start(message):
-	bot.send_message(message.chat.id, "Hello!")
+	bot.send_message(message.chat.id, "Hi")
 
-@app.route('/webhook', methods=['POST'])
+@server.route('/' + TOKEN, methods=['POST'])
+def getMessage():
+    json_string = request.get_data().decode('utf-8')
+    update = telebot.types.Update.de_json(json_string)
+    bot.process_new_updates([update])
+    return "!", 200
+    
+@server.route("/")
 def webhook():
-     update = telebot.types.Update.de_json(
-            request.stream.read().decode("utf-8"))
-     bot.process_new_updates([update])
-     return "ok", 200
+    bot.remove_webhook()
+    bot.set_webhook(url='https://flask1.tolasaa.repl.co/' + TOKEN)
+    return "ok", 200
+
+if __name__ == "__main__":
+    server.run(host="0.0.0.0", port=int(os.environ.get('PORT', 5000)))
